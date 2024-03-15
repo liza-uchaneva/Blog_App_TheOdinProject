30f9a38e426a2cf70c62aa65248879fb246775e0
Elizaveta Uchaneva <liza.uchaneva@outlook.com>
2024-03-15 16:16:45 -0700

    Made a Blog App for The Odin Project

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+end
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
+class Article < ApplicationRecord
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
